c1dbfe467d8fa7ea276617131f33944e6168fe66
vine_dasha_ <56861606+vinedasha@users.noreply.github.com>
2022-05-22 16:24:41 +0300

    Add files via upload

---
+++ диплом 22.05.22 16-23/portfol/portfolio/check.js
@@ -0,0 +1,96 @@
+function request(url)
+{
+	let r = new XMLHttpRequest();
+	r.open('GET', url, false);
+	r.send();
+	return r.responseText;
+}
+window.addEventListener('load', main, false);
+
+function main()
+{
+	var btn = document.getElementById('det');
+	btn.addEventListener('click',checkdet,false);
+        var bt = document.getElementById('doc');
+	bt.addEventListener('click',checkdoc,false);
+        var b = document.getElementById('achi');
+	b.addEventListener('click',checkachi,false);
+}
+
+function checkdet()
+{
+    let id_t = parseGetParams().teach_id;
+    let id_s = document.getElementById('id_stud');
+    let pl = document.getElementById('notstud');
+    
+    document.getElementById('notstud').textContent = '';
+   
+    let res = parseInt((request('http://portfol/portfolio/id_s.php?id_stud='+id_s.value)).replace(/\D+/g,''));
+    var t = res.toString();
+    var id = parseInt(t, 10);
+    if (id != id_t)
+    {               
+            var text = document.createTextNode('Вы хотите посмотреть подробности не своего студента');
+            pl.appendChild(text);
+    }
+       else  if (res != null && res.length != 0 && id == id_t)
+	{	
+                window.open("http://portfol/detail/detail.php?id_stud="+id_s.value, "_self");
+	}
+}
+
+function checkdoc()
+{
+    let id_t = parseGetParams().teach_id;
+    let id_s = document.getElementById('id_stud');
+    let pl = document.getElementById('notstud');
+    
+    document.getElementById('notstud').textContent = '';
+   
+    let res = parseInt((request('http://portfol/portfolio/id_s.php?id_stud='+id_s.value)).replace(/\D+/g,''));
+    var t = res.toString();
+    var id = parseInt(t, 10);
+    if (id != id_t)
+    {               
+            var text = document.createTextNode('Вы хотите посмотреть документы не своего студента');
+            pl.appendChild(text);
+    }
+       else  if (res != null && res.length != 0 && id == id_t)
+	{	
+                window.open("http://portfol/doc/doc.php?id_stud="+id_s.value, "_self");
+	}
+}
+
+function checkachi()
+{
+    let id_t = parseGetParams().teach_id;
+    let id_s = document.getElementById('id_stud');
+    let pl = document.getElementById('notstud');
+    
+    document.getElementById('notstud').textContent = '';
+   
+    let res = parseInt((request('http://portfol/portfolio/id_s.php?id_stud='+id_s.value)).replace(/\D+/g,''));
+    var t = res.toString();
+    var id = parseInt(t, 10);
+    if (id != id_t)
+    {               
+            var text = document.createTextNode('Вы хотите посмотреть подробности не своего студента');
+            pl.appendChild(text);
+    }
+       else  if (res != null && res.length != 0 && id == id_t)
+	{	
+                window.open("http://portfol/achi/achi.php?id_stud="+id_s.value, "_self");
+	}
+}
+
+
+function parseGetParams() { 
+   var $_GET = {}; 
+   var __GET = window.location.search.substring(1).split("&"); 
+   for(var i=0; i<__GET.length; i++) { 
+      var getVar = __GET[i].split("="); 
+      $_GET[getVar[0]] = typeof(getVar[1])=="undefined" ? "" : getVar[1]; 
+   } 
+   return $_GET; 
+}
+
---
+++ диплом 22.05.22 16-23/portfol/reg/log.js
@@ -0,0 +1,27 @@
+function request(url)
+{
+	let r = new XMLHttpRequest();
+	r.open('GET', url, false);
+	r.send();
+	return r.responseText;
+}
+function do_stuff()
+{
+	let blk = document.getElementById('fio');
+	let input = document.getElementsByTagName('input')[0];
+	let input1 = document.getElementsByTagName('input')[1];
+	let input2 = document.getElementsByTagName('input')[2];
+        let input3 = document.getElementsByTagName('input')[3];
+        let input4 = document.getElementsByTagName('input')[4];
+
+	let res = request('cont.php?logins='+input.value + '&' + 'passwords='+input1.value + '&' + 'su='+input2.value + '&' + 'na='+input3.value + '&' + 'pa='+input4.value );
+
+}
+
+function main()
+{
+	var btn = document.getElementById('select');
+	btn.addEventListener('click',do_stuff,false);
+}
+
+window.addEventListener('load', do_stuff, false);
